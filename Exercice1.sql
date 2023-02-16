f216de6126c99e4f38f085e8356f6b91968c3438
SORO <kolosiaka@gmail.com>
2023-02-16 21:11:02 +0100

    'w8d2'

---
+++ Exercice1.sql
@@ -0,0 +1,4 @@
+--SELECT * FROM items ORDER BY price ASC ;
+--SELECT * FROM items ORDER BY price >= 80 , price DESC;
+--SELECT id,name FROM customers ORDER BY name ASC LIMIT 3;
+--SELECT name FROM customers ORDER BY name DESC ;
